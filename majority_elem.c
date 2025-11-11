b60275bfd4c3bd77143bdc629b65543967e0c083
Junaid <alinajma109@gmail.com>
2025-11-11 19:32:33 +0530

    13th commit

---
+++ majority_elem.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int arr[20];
+    int n;
+    printf("Enter the numbers having similar in:");
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    
+}
