bff7123a5a14b36ed62bef7a9fb0cb9dcc1da399
Junaid <alinajma109@gmail.com>
2025-11-12 14:38:43 +0530

    13th commit

--- majority_elem.c
+++ majority_elem.c
@@ -1,11 +1,30 @@
-#include<stdio.h>
+ #include<stdio.h>
 int main(){
-    int arr[20];
+
+    int freq=0 ,ans=0 ;
     int n;
-    printf("Enter the numbers having similar in:");
+    printf("Enter the size of array:");
+    scanf("%d",&n);
+    int arr[n];
+    printf("Enter the numbers having similar in:\n");
     for (int i = 0; i < n; i++)
     {
         scanf("%d",&arr[i]);
     }
-    
+    printf("Numbers are:\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d",arr[i]);
+    }
+
+    for(int i =0 ; i<n; i++){
+        if(freq == 0){
+            ans=arr[i];
+        }
+        if(ans==arr[i])
+        freq++;
+        else
+        freq--;
+    }
+    printf("\nmajority element: %d",ans);
 }
